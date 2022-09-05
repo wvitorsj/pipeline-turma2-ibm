66346804b6601c349afcade768284e0607ec0a0a
Welton Vitor <wvitorsj08@gmail.com>
2022-09-05 09:02:49 -0300

    Subindo JAVA na Pipeline

---
+++ src/main/java/br/com/wvitorcorp/rodandoTesteGitLab/RodandoTesteGitLabApplication.java
@@ -0,0 +1,13 @@
+package br.com.wvitorcorp.rodandoTesteGitLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodandoTesteGitLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodandoTesteGitLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wvitorcorp/rodandoTesteGitLab/RodandoTesteGitLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wvitorcorp.rodandoTesteGitLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodandoTesteGitLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
